278200959d7a3a6b2b8ff1d83713dc1d87aba0d9
Alice <yuu190825@icloud.com>
2023-03-05 19:53:04 +0800

    Initial Commit

---
+++ Calc/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Calc
+//
+//  Created by 王ユウ on 2023/03/05.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
